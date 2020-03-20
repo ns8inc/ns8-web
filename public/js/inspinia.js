90caf5106c493b6b87caaf00d9ea5a4a883d128b
vizzaccaro <phil@netapplications.com>
2020-03-20 13:34:43 -0700

    localStorage error trap required with new chrome

--- public/js/inspinia.js
+++ public/js/inspinia.js
@@ -167,46 +167,50 @@ $(window).bind("resize", function () {
 $(document).ready(function () {
     if (localStorageSupport) {
 
-        var collapse = localStorage.getItem("collapse_menu");
-        var fixedsidebar = localStorage.getItem("fixedsidebar");
-        var fixednavbar = localStorage.getItem("fixednavbar");
-        var boxedlayout = localStorage.getItem("boxedlayout");
-        var fixedfooter = localStorage.getItem("fixedfooter");
+        try {
+            var collapse = localStorage.getItem("collapse_menu");
+            var fixedsidebar = localStorage.getItem("fixedsidebar");
+            var fixednavbar = localStorage.getItem("fixednavbar");
+            var boxedlayout = localStorage.getItem("boxedlayout");
+            var fixedfooter = localStorage.getItem("fixedfooter");
 
-        var body = $('body');
+            var body = $('body');
 
-        if (fixedsidebar == 'on') {
-            body.addClass('fixed-sidebar');
-            $('.sidebar-collapse').slimScroll({
-                height: '100%',
-                railOpacity: 0.9
-            });
-        }
-
-        if (collapse == 'on') {
-            if (body.hasClass('fixed-sidebar')) {
-                if (!body.hasClass('body-small')) {
-                    body.addClass('mini-navbar');
-                }
-            } else {
-                if (!body.hasClass('body-small')) {
-                    body.addClass('mini-navbar');
-                }
-
+            if (fixedsidebar == 'on') {
+                body.addClass('fixed-sidebar');
+                $('.sidebar-collapse').slimScroll({
+                    height: '100%',
+                    railOpacity: 0.9
+                });
             }
-        }
 
-        if (fixednavbar == 'on') {
-            $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
-            body.addClass('fixed-nav');
-        }
+            if (collapse == 'on') {
+                if (body.hasClass('fixed-sidebar')) {
+                    if (!body.hasClass('body-small')) {
+                        body.addClass('mini-navbar');
+                    }
+                } else {
+                    if (!body.hasClass('body-small')) {
+                        body.addClass('mini-navbar');
+                    }
 
-        if (boxedlayout == 'on') {
-            body.addClass('boxed-layout');
-        }
+                }
+            }
 
-        if (fixedfooter == 'on') {
-            $(".footer").addClass('fixed');
+            if (fixednavbar == 'on') {
+                $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
+                body.addClass('fixed-nav');
+            }
+
+            if (boxedlayout == 'on') {
+                body.addClass('boxed-layout');
+            }
+
+            if (fixedfooter == 'on') {
+                $(".footer").addClass('fixed');
+            }
+        } catch(err) {
+            // probably here due to chrome 3rd party localStorage issue
         }
     }
 });
